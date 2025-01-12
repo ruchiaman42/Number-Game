8e81cdf29c68a77093bc2a3a4f70d1f80c6f6359
Aman <ruchiaman42@gmail.com>
2025-01-12 15:35:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
+
+const compareGuesses = (humanGuess, computerGuess, secretTargetNumber) => {
+humanGuessDifference = Math.abs(humanGuess - secretTargetNumber)
+computerGuessDifference = Math.abs(computerGuess - secretTargetNumber)
+if(humanGuessDifference <= computerGuessDifference) {
+    return true
+} else {
+    return false
+}
+}
+
+const updateScore = (winner) => {
+    if (winner === "human") {
+        humanScore++
+    } else {
+        computerScore++
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++
+}
